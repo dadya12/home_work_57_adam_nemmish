f0c26d48e0648669d0f24488b5f258549926db4a
Adam <adam.nemmish213@gmail.com>
2023-12-15 00:54:19 +0600

    Создал модели задач, типов, статусов. Накатил миграцию и применил ее, зарегал в админке. Создал админку добавил пару записей. Создал фикстуры

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from webapp.models import Type, Status, Task
 
-# Register your models here.
+@admin.register(Type)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['id', 'name']
+    search_fields = ['id', 'name']
+    fields = ['name']
+@admin.register(Status)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['id', 'name']
+    search_fields = ['id', 'name']
+    fields = ['name']
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'description', 'status', 'type', 'created_date', 'updated_date']
+    list_filter = ['id', 'status', 'type']
+    search_fields = ['id', 'summary']
+    fields = ['summary', 'description', 'status', 'type', 'created_date', 'updated_date']
+    readonly_fields = ['created_date', 'updated_date']
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Type(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=150)
+
+class Status(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=150)
+
+class Task(models.Model):
+    summary = models.CharField(verbose_name='Краткое описание', max_length=200)
+    description = models.TextField(verbose_name='Полное описание', max_length=500, null=True, blank=True)
+    status = models.ForeignKey('webapp.Status', verbose_name='Статус', on_delete=models.RESTRICT)
+    type = models.ForeignKey('webapp.Type', verbose_name='Тип', on_delete=models.RESTRICT)
+    created_date = models.DateTimeField(verbose_name='Дата и время создания', auto_now_add=True)
+    updated_date = models.DateTimeField(verbose_name='Дата и время обновления', auto_now=True)
+
