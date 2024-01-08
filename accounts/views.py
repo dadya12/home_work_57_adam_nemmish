b22e549404dd168e727f03a72e9b07626dc23a59
Adam <adam.nemmish213@gmail.com>
2024-01-08 15:53:46 +0600

    Добавил возможность регистрации нового пользователя

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,26 @@
-from django.contrib.auth import login, authenticate, logout
-from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout, get_user_model
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
+from accounts.forms import MyUserCreationForm
 
 
-# Create your views here.
+class RegistrationView(CreateView):
+    model = get_user_model()
+    form_class = MyUserCreationForm
+    template_name = 'create_user.html'
 
-def login_view(request):
-    context = {}
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('webapp:home')
-        else:
-            context['has_error'] = True
-    return render(request, 'login.html', context=context)
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
 
-def logout_view(request):
-    logout(request)
-    return redirect('webapp:home')
+    def get_success_url(self):
+        next_page = self.request.GET.get('next')
+        if not next_page:
+            next_page = self.request.POST.get('next')
+        if not next_page:
+            next_page = reverse('webapp:home')
+
+        return next_page
+
